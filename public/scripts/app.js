2ac3bf0f10ee8343f5e1a143760710c9b175e456
mariaxtina <mchristinahess@gmail.com>
2016-07-12 12:45:25 -0700

    Edit format for redundancy and readability

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,67 @@
 function createTweetElement(tweet) {
-    return $(
-      `<article class='tweet'>
+  return $(
+    `<article class='tweet'>
       <header>
         <img class='avatar' src=${tweet.user.avatars.small}>
         <h1>${tweet.user.name}</h1>
         <span class="handle">${tweet.user.handle}</span>
       </header>
-      <div>
-       ${tweet.content.text}
-      </div>
+      <div class="tweet-body">${$('<div/>').text(tweet.content.text).html()}</div>
       <footer>
-        <span> ${tweet.created_at} </span>
+        <span class="date"> ${tweet.created_at} </span>
         <a href="#" class="button"><i class="fa fa-flag" aria-hidden="true"></i></a>
         <a href="#" class="button"> <i class="fa fa-retweet" aria-hidden="true"></i> </a>
         <a href="#" class="button"><i class="fa fa-heart" aria-hidden="true"></i></a>
       </footer>
-     </article>`);
+    </article>`);
+}
 
-  }
+function renderTweets(tweets) {
+  tweets.forEach(function(tweet) {
+    createTweetElement(tweet).appendTo('#all-tweets');
+  });
+}
 
-  function renderTweets(tweets) {
-    tweets.forEach(function(tweet) {
-      createTweetElement(tweet).appendTo('#all-tweets');
-    });
-  }
+window.TWEET_CHAR_LIMIT = 140;
 
 $(document).ready(function() {
 
-  // getAndRenderAllTweets();
+  getAndRenderAllTweets();
 
-    $("form").on( "submit", function(event) {
-      event.preventDefault();
-      var newTweet = ($(this).serialize());
+  $("form").on( "submit", function(event) {
+    event.preventDefault();
+    let newTweet = ($(this).serialize());
 
-      if ($('textarea').val() == "" || null) {
-        alert("Please enter text!");
-        return false;
-      } else if ($('textarea').val().length > 140) {
-          alert("Too long!");
-          return false;
-      }
+    if ($('textarea').val() == "" || null) {
+      alert("Please enter text!");
+      return false;
+    } else if ($('textarea').val().length > TWEET_CHAR_LIMIT) {
+      alert("Too long!");
+      return false;
+    }
 
-      let inputText = $("form").find("input[type=text], textarea").val("");
+    let inputText = $("form").find("input[type=text], textarea").val("");
 
-      console.log(inputText);
-
-      $.ajax({
-          url: '/tweets/',
-          method: 'POST',
-          data: newTweet,
-          dataType: "json",
-          success: onTweetSuccess
-        });
-        // error.function() {
-        //   alert('error')
-        // }
-    });
-  });
-
-  var onTweetSuccess = function(data) {
-    getAndRenderAllTweets();
-  }
-
-  var getAndRenderAllTweets = function() {
     $.ajax({
         url: '/tweets/',
-        method: 'GET',
-        success: function (data) {
-          $(".tweet").remove();
-          renderTweets(data);
-        }
-      });
+        method: 'POST',
+        data: newTweet,
+        dataType: "json",
+        success: getAndRenderAllTweets()
+    });
+
+  });
+
+});
+
+
+const getAndRenderAllTweets = function() {
+  $.ajax({
+    url: '/tweets/',
+    method: 'GET',
+    success: function (data) {
+      $(".tweet").remove();
+      renderTweets(data);
+    }
+  });
 }
