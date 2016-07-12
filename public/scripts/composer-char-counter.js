0a4cac9fea7f1b1091fa93a7d0757e423af587ad
mariaxtina <mchristinahess@gmail.com>
2016-07-12 12:44:34 -0700

    Add magic number

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
+
   $('.new-tweet textarea').on("keyup", function() {
-    var charsRemaining = 140 - $(this).val().length;
+
+    let charsRemaining = TWEET_CHAR_LIMIT - $(this).val().length;
+
     $(".counter").html(charsRemaining);
     if (charsRemaining < 0) {
       $(".counter").addClass("over-limit");
     } else {
       $(".counter").removeClass("over-limit");
     }
+
   });
+
 });
